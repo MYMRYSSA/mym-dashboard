cdc47e957c272ec5980bcf1f6e3f1330c3089b4e
Camilo Taipe <ctaipe@quantum-talent.com>
2022-02-13 13:48:07 -0500

    feat: added https for nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    useFileSystemPublicRoutes: false,
     experimental: {
         outputStandalone: true
     },
---
+++ server.js
@@ -0,0 +1,37 @@
+const https = require('https');
+const { parse } = require('url');
+const fs = require('fs');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+
+const options = {
+  key: fs.readFileSync('./certs/ssl.key'),
+  cert: fs.readFileSync('./certs/ssl.crt'),
+  // ca: [fs.readFileSync('./certs/root.crt')]
+};
+
+app.prepare().then(() => {
+  https
+    .createServer(options, async (req, res) => {
+      try {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+        await app.render(req, res, pathname, query);
+      } catch (err) {
+        console.error('Error occurred handling', req.url, err);
+        res.statusCode = 500;
+        res.end('internal server error');
+      }
+    })
+    .listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on https://${hostname}:${port}`);
+    });
+});
